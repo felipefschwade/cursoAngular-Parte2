6e488b41bde96baba7000face1968cbcdcc794b9
Felipe F. Schwade <Felipe F. Schwade>
2017-03-02 16:01:07 -0400

    Importando o prodMode

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode } from "@angular/core";
 import { AppModule } from "./app.module";
 
 //Cria a um instância de platformBrowserDynamic para carregar o primeiro modulo da app e o restante em seguida
 const platform = platformBrowserDynamic();
 //Define como modulo principal da app o AppModule criando anteriormente
+// enableProdMode();
 platform.bootstrapModule(AppModule);
